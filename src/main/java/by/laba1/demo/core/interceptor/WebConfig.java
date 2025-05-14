e6ae31b792b31835c96ec7a87449faf0d6955fd7
MegaXRL <zaxarrim@gmail.com>
2025-05-14 10:06:23 +0300

    lab7_seventhChanges

--- src/main/java/by/laba1/demo/core/interceptor/WebConfig.java
+++ src/main/java/by/laba1/demo/core/interceptor/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -24,10 +25,16 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOrigins("http://localhost:3000") // React порт
+            .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
     }

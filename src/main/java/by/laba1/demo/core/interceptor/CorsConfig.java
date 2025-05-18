b4f915bfaaa0dff51c5238b6158b2cbda5b6b974
MegaXRL <zaxarrim@gmail.com>
2025-05-18 18:07:54 +0300

    lab8_RenderFull

--- src/main/java/by/laba1/demo/core/interceptor/CorsConfig.java
+++ src/main/java/by/laba1/demo/core/interceptor/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed.origins:http://localhost,http://localhost:80,http://localhost:3000}")
+    @Value("${cors.allowed.origins:http://localhost,http://localhost:80,http://localhost:3000,https://demolab1-frontend-latest.onrender.com}")
     private String allowedOrigins;
 
     @Bean
